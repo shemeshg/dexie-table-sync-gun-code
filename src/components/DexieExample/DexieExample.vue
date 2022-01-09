4f515d96e6739da6f4de8862d0be1011a4b7d86b
osx user <gili.shemesh@gmail.com>
2022-01-09 19:35:48 +0200

    commit

--- src/components/DexieExample/DexieExample.vue
+++ src/components/DexieExample/DexieExample.vue
@@ -1,13 +1,24 @@
 <template>
   <p v-if="!isIpfsReady">Loading IPFS...</p>
   <div v-if="isIpfsReady">
-    <p v-if="isSyncDefined">Sync with {{ syncUrl }}</p>
-    <button v-if="isSyncDefined" @click="doUndefineSync">undefine sync</button>
-    <input type="text" v-if="!isSyncDefined" v-model="orbitdbUrlToOpen" />
-    <button v-if="!isSyncDefined" @click="doDefineSync">Start Gun</button>
+    <p>Status {{ syncUrl }}</p>
+    <button v-if="isSyncDefined" @click="doLogout">Logout gun</button>
+    <div v-if="!isSyncDefined">
+      User name:
+      <input type="text" v-if="!isSyncDefined" v-model="gunStoreUser" />
+      Password:
+      <input type="text" v-if="!isSyncDefined" v-model="gunStorePassword" />
+    </div>
+    <button v-if="!isSyncDefined" @click="doAuthenticate">
+      Login and sync Gun
+    </button>
+    <button v-if="!isSyncDefined" @click="doRegisterUser">
+      Register new user
+    </button>
     <div v-if="isSyncDefined">
       <p>
-          If you want to recreate the Gun Store, or import a new store
+        If you want to recreate the Gun Store, or import astore just created new
+        store
       </p>
       <p>
         <button @click="importCurrentDixieTableToGun">
@@ -22,50 +33,73 @@
 <script lang="ts">
 import FriendsList from "@/components/DexieExample/FriendsList.vue"; // @ is an alias to /src
 import { defineComponent, ref, onMounted } from "vue";
-
+import { gunAuth } from "./GunHelper";
 
 export default defineComponent({
   props: {},
   components: { FriendsList },
   setup() {
-    const friendsListRef=ref()
+    const friendsListRef = ref();
     const isIpfsReady = ref(false);
     const isSyncDefined = ref(false);
     const syncUrl = ref("");
-    const orbitdbUrlToOpen = ref("");
-
+    const gunStoreUser = ref("");
+    const gunStorePassword = ref("");
     const doOnMounted = async () => {
+      isSyncDefined.value = gunAuth.isLoggedIn;
       isIpfsReady.value = true;
     };
 
-    const doUndefineSync = async () => {
+    const doLogout = async () => {
+      gunAuth.gunLogout();
       isSyncDefined.value = false;
     };
 
+    const doAuthenticate = async () => {
 
-
-    const doDefineSync = async () => {
-      await doUndefineSync();
+      try {
+        (await gunAuth.gunAuthUser(
+          gunStoreUser.value,
+          gunStorePassword.value
+        )) as string;
+        syncUrl.value = "User authenticated";
+      } catch (e) {
+        syncUrl.value = e as string;
+        return;
+      }
 
       isSyncDefined.value = true;
       syncUrl.value = "";
-
     };
     const importCurrentDixieTableToGun = async () => {
       friendsListRef.value.importCurrentDixieTableToGun();
-      
+    };
+
+    const doRegisterUser = async () => {
+      try {
+        (await gunAuth.gunRegisterUser(
+          gunStoreUser.value,
+          gunStorePassword.value
+        )) as string;
+        syncUrl.value = "User created, please login";
+      } catch (e) {
+        syncUrl.value = e as string;
+        return;
+      }
     };
     onMounted(doOnMounted);
 
     return {
       isSyncDefined,
-      doUndefineSync,
-      doDefineSync,
+      doLogout,
+      doAuthenticate,
       syncUrl,
-      orbitdbUrlToOpen,
+      gunStoreUser,
       isIpfsReady,
       importCurrentDixieTableToGun,
       friendsListRef,
+      gunStorePassword,
+      doRegisterUser,
     };
   },
 });
