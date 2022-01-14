b92473bb9039de1322589b1f5f5517ba827c431a
osx user <gili.shemesh@gmail.com>
2022-01-14 14:45:17 +0200

    commit

--- src/components/DexieExample/DexieExample.vue
+++ src/components/DexieExample/DexieExample.vue
@@ -1,21 +1,21 @@
 <template>
   <div>
-    <p>Status {{ syncUrl }}</p>
-    <div >
+    <p>
+      <a href="https://github.com/shemeshg/dexie-table-sync-gun-code"
+        >https://github.com/shemeshg/dexie-table-sync-gun-code</a
+      >
+    </p>
+    <div>
       Application Id:
-      <input type="text"  v-model="gunApplicationId" :readonly="isSyncDefined" />
-      <p> Start application, with same AppId in two different browsers </p>
+      <input type="text" v-model="gunApplicationId" :readonly="isSyncDefined" />
+      <p>Start application, with same AppId in two different browsers</p>
     </div>
-    <button v-if="!isSyncDefined" @click="doStart">
-      Start 
-    </button>   
+    <button v-if="!isSyncDefined" @click="doStart">Start</button>
     <div v-if="isSyncDefined">
       <p>
-        If you want to recreate the Gun Store, or import astore from local table        
+        If you want to recreate the Gun Store, or import astore from local table
       </p>
-      <p>
-        The sync from Gun connected store to Dexie is Auto
-      </p>
+      <p>The sync from Gun connected store to Dexie is Auto</p>
       <p>
         <button @click="importCurrentDixieTableToGun">
           Import current DixieTable to Gun
@@ -30,22 +30,20 @@
 import FriendsList from "@/components/DexieExample/FriendsList.vue"; // @ is an alias to /src
 import { defineComponent, ref } from "vue";
 
-import {setGunApp} from "./GunApp"
+import { setGunApp } from "./GunApp";
 
 export default defineComponent({
   props: {},
   components: { FriendsList },
-  setup() {    
-
+  setup() {
     const friendsListRef = ref();
     const isSyncDefined = ref(false);
-  
+
     const gunApplicationId = ref("");
-  
 
     const doStart = async () => {
-      setGunApp(gunApplicationId.value)
-      isSyncDefined.value=true
+      setGunApp(gunApplicationId.value);
+      isSyncDefined.value = true;
     };
     const importCurrentDixieTableToGun = async () => {
       friendsListRef.value.importCurrentDixieTableToGun();
